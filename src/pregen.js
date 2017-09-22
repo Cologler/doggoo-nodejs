eddf515fe2c401043dccde7c1ae02ea5973e08eb
skyoflw <skyoflw@gmail.com>
2017-09-23 02:58:07 +0800

    batch download images.

--- src/pregen.js
+++ src/pregen.js
@@ -7,6 +7,7 @@ const { ImageElement } = require('./model');
 
 async function prepareNovel(context) {
     let index = 0;
+    const promises = [];
     for (const chapter of context.novel.chapters) {
         const images = chapter.contents.filter(z => z instanceof ImageElement);
         if (images) {
@@ -14,21 +15,33 @@ async function prepareNovel(context) {
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
             }
-            for (const img of images) {
+            for (const item of images) {
+                const img = item;
                 const url = img.url;
-                const response = await bhttp.get(url, {
-                    steam: true
-                });
                 const filename = index + PATH.extname(url);
                 const path = PATH.join(dir, filename);
-                fs.writeFileSync(path, response.body, {
-                    encoding: 'binary',
-                    flag: 'w'
-                });
-                img.path = PATH.join('.', 'images', filename);
                 index++;
+
+                const promise = bhttp.get(url, {
+                    steam: true
+                });
+                promises.push({
+                    promise,
+                    callback: response => {
+                        fs.writeFileSync(path, response.body, {
+                            encoding: 'binary',
+                            flag: 'w'
+                        });
+                        img.path = PATH.join('images', filename);
+                        index++;
+                    }
+                });
             }
         }
+    }
+    for (const p of promises) {
+        const r = await p.promise;
+        p.callback(r);
     }
 }
 
