f686adc575679b38615c2f938d1b77da4aead404
skyoflw <skyoflw@gmail.com>
2017-09-23 03:13:06 +0800

    refactor the Preproccesser

--- src/pregen.js
+++ src/pregen.js
@@ -5,44 +5,55 @@ const fs = require('fs');
 var bhttp = require("bhttp");
 const { ImageElement } = require('./model');
 
-async function prepareNovel(context) {
-    let index = 0;
-    const promises = [];
-    for (const chapter of context.novel.chapters) {
-        const images = chapter.contents.filter(z => z instanceof ImageElement);
-        if (images) {
-            const dir = PATH.join(context.root, 'images');
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
-            for (const item of images) {
-                const img = item;
-                const url = img.url;
-                const filename = index + PATH.extname(url);
-                const path = PATH.join(dir, filename);
-                index++;
+class Preproccesser {
+    constructor() {
+        this._promises = [];
+    }
 
-                const promise = bhttp.get(url, {
-                    steam: true
-                });
-                promises.push({
-                    promise,
-                    callback: response => {
-                        fs.writeFileSync(path, response.body, {
-                            encoding: 'binary',
-                            flag: 'w'
-                        });
-                        img.path = PATH.join('images', filename);
-                        index++;
-                    }
-                });
+    prepare(context) {
+        let index = 0;
+        for (const chapter of context.novel.chapters) {
+            const images = chapter.contents.filter(z => z instanceof ImageElement);
+            if (images) {
+                const dir = 'asserts';
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir);
+                }
+                for (const img of images) {
+                    this.onImage(dir, index, img);
+                    index++;
+                }
             }
         }
     }
-    for (const p of promises) {
-        const r = await p.promise;
-        p.callback(r);
+
+    async onImage(root, index, img) {
+        const url = img.url;
+        const filename = index + PATH.extname(url);
+        const path = PATH.join(root, filename);
+
+        const promise = bhttp.get(url, {
+            steam: true,
+            responseTimeout: 5000
+        });
+        this._promises.push(promise);
+        const response = await promise;
+        fs.writeFileSync(path, response.body, {
+            encoding: 'binary',
+            flag: 'w'
+        });
+        img.path = path;
     }
+
+    async waitAll() {
+        return Promise.all(this._promises);
+    }
+}
+
+async function prepareNovel(context) {
+    const per = new Preproccesser();
+    per.prepare(context);
+    await per.waitAll();
 }
 
 module.exports = {
