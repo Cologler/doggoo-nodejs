7eaa3e190537e8bb713f0960d6cb142ab53fb939
skyoflw <skyoflw@gmail.com>
2017-09-23 15:12:51 +0800

    add epub generator

--- src/generators.js
+++ src/generators.js
@@ -111,15 +111,93 @@ class MarkdownGenerator extends Generator {
     }
 }
 
+
+const EpubGenerator = (() => {
+    const uuid = require('node-uuid');
+    const xmlescape = require('xml-escape');
+
+    class EpubGenerator extends Generator {
+        constructor () {
+            super();
+            this._book = require("epub-builder");
+            this._cover = null;
+            this._imageIndex = 0;
+        }
+
+        generate(context) {
+            const novel = context.novel;
+            const book = this._book;
+            const title = novel.title || 'NOVEL';
+
+            book.setTitle(title);
+            book.setAuthor(novel.author || 'AUTHOR');
+            book.setSummary(novel.summary || 'SUMMARY');
+            book.setUUID(uuid.v4());
+
+            this._cover = context.args['--cover'] || null;
+            if (this._cover) {
+                const ci = Number(this._cover);
+                if (!isNaN(ci)) {
+                    this._cover = ci;
+                }
+            }
+
+            novel.chapters.forEach((z, i) => {
+                const text = this.toDoc(z).trim();
+                book.addChapter("Chapter Title", text);
+            });
+            book.createBook(title);
+        }
+
+        onLineBreak(node) {
+            return ''; // epub does not allow <br/>
+        }
+
+        onTextElement(node) {
+            const data = xmlescape(node.content);
+            if (node.textIndex === 0) {
+                return `<h1>${data}</h1>`;
+            } else {
+                return `<p>${xmlescape(node.content)}</p>`;
+            }
+        }
+
+        onImageElement(node) {
+            if (this._imageIndex === this._cover || node.url === this._cover) {
+                this._book.addCoverImage(node.path);
+            } else {
+                this._book.addAsset(node.path);
+            }
+
+            this._imageIndex++;
+            return `<img src="${node.filename}"/>`;
+        }
+
+        onLinkElement(node) {
+            const data = xmlescape(node.title);
+            return `<a href="${node.url}">${data}</a>`;
+        }
+    }
+
+    return EpubGenerator;
+})();
+
+
 function getGenerator(name) {
     switch (name) {
         case 'markdown':
         case 'md':
         case 'gitbook':
             return new MarkdownGenerator();
+
         case 'txt':
-        default:
             return new TxtGenerator();
+
+        case 'epub':
+            return new EpubGenerator();
+
+        default:
+            return new EpubGenerator();
     }
 }
 
