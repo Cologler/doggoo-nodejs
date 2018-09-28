e11619016225ad9ccfea45a8fb5f694a9d3eaff7
Cologler <skyoflw@gmail.com>
2018-09-29 01:31:18 +0800

    remove unused log

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { ioc } from "anyioc";
 
 import { AppInfo } from "./doggoo";
 
-const build = 48;
-const buildtime = new Date(1538001209484);
+const build = 49;
+const buildtime = new Date(1538155709823);
 
 ioc.registerSingleton('app-info', () => {
     const info: AppInfo = {
