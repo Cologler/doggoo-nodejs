6721fe1c121a68b3e07dc1db6f2cbc633bf18d27
Cologler <skyoflw@gmail.com>
2019-07-11 13:17:55 +0800

    add tsc

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { ioc } from "anyioc";
 
 import { AppInfo } from "./doggoo";
 
-const build = 53;
-const buildtime = new Date(1540890274437);
+const build = 55;
+const buildtime = new Date(1562822197364);
 
 ioc.registerSingleton('app-info', () => {
     const info: AppInfo = {
